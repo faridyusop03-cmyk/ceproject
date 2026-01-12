3d43e30600a75d8d140609fd888f1cf1e01144f0
faridyusop03-cmyk <faridyusop03@gmail.com>
2026-01-12 16:29:09 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Load your dataset
-df = pd.read_csv('/mnt/data/project_benchmark_data_ce.csv')
+df = pd.read_csv('project_benchmark_data_ce.csv')
 
 # Check the first few rows of the dataset
 print(df.head())
